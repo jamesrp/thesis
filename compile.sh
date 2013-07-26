8e07cef37c142ded887f11ea762f7448c47d654e
James Pfeiffer <jamesrpfeiffer@gmail.com>
2013-07-26 12:18:28 -0700

    Temporarily remove citations in chapter titles

--- compile.sh
+++ compile.sh
@@ -4,4 +4,4 @@ pdflatex thesis.tex
 pdflatex thesis.tex
 #dvipdf thesis.dvi
 # Now that I'm using the correct commands, presumably this isn't needed.
-rm *.aux *.bbl *.blg *.log *.toc *.dvi
+rm *.aux *.bbl *.blg *.log *.toc
