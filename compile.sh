7c5d137f8efc8d0e46bbfd9faccb5d3af6ed8f48
James Pfeiffer <jamesrpfeiffer@gmail.com>
2013-07-25 21:04:17 -0700

    Fix compile script to use bibtex correctly

--- compile.sh
+++ compile.sh
@@ -1,4 +1,7 @@
-rm *.log *.aux *.blg *.bbl
-pdflatex pfeiffer-thesis.tex
-pdflatex pfeiffer-thesis.tex
-
+latex pfeiffer-thesis.tex
+bibtex pfeiffer-thesis
+latex pfeiffer-thesis.tex
+latex pfeiffer-thesis.tex
+dvipdf pfeiffer-thesis.dvi
+# Now that I'm using the correct commands, presumably this isn't needed.
+# rm *.aux *.bbl *.blg *.log *.toc 
