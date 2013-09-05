9418b0e3039dfeaa8d423e73e4606e745f329a3a
James Pfeiffer <jamesrpfeiffer@gmail.com>
2013-09-05 16:35:07 -0700

    Added compile.py

---
+++ compile.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess, sys
+
+if sys.argv[1] == '-f':
+    subprocess.call(["pdflatex", "thesis.tex"])
+    subprocess.call(["bibtex", "thesis"])
+    subprocess.call(["pdflatex", "thesis.tex"])
+    subprocess.call(["pdflatex", "thesis.tex"])
+    print "Compiled thesis.tex with full compile."
+else:
+    # Assume stub
+    subprocess.call(["pdflatex", "stub.tex"])
+    print "Compiled stub.tex."
+
+exts = ["aux","bbl","blg","log","toc","out"]
+
+import os
+files = os.listdir('.')
+for fname in files:
+    if any(ext in fname for ext in exts):
+        os.remove(fname)
+
+print "Removed temporary files."
--- compile.sh
+++
@@ -1,7 +0,0 @@
-pdflatex thesis.tex
-bibtex thesis
-pdflatex thesis.tex
-pdflatex thesis.tex
-#dvipdf thesis.dvi
-# Now that I'm using the correct commands, presumably this isn't needed.
-rm *.aux *.bbl *.blg *.log *.toc *.out
