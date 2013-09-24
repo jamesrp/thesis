b0a072ef6b7cb21b4b161f502ca2f83ac941d5e5
James Pfeiffer <jamesrpfeiffer@gmail.com>
2013-09-24 11:33:56 -0700

    Updated compilation script to remove stub option

--- compile.py
+++ compile.py
@@ -8,15 +8,10 @@ for fname in files:
     if any(ext in fname for ext in exts):
         os.remove(fname)
 
-if len(sys.argv) > 1: # heh
-    subprocess.call(["pdflatex", "thesis.tex"])
-    subprocess.call(["bibtex", "thesis"])
-    subprocess.call(["pdflatex", "thesis.tex"])
-    subprocess.call(["pdflatex", "thesis.tex"])
-    print "Compiled thesis.tex with full compile."
-else:
-    # Assume stub
-    subprocess.call(["pdflatex", "stub.tex"])
-    print "Compiled stub.tex."
+subprocess.call(["pdflatex", "thesis.tex"])
+subprocess.call(["bibtex", "thesis"])
+subprocess.call(["pdflatex", "thesis.tex"])
+subprocess.call(["pdflatex", "thesis.tex"])
+print "Compiled thesis.tex with full compile."
 
 
